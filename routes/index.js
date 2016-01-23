a3c2ca961f15c057caf24cfebb73a10b7a9dad8e
Eduardo <eduardo.s.tanaka@gmail.com>
2016-01-23 20:35:06 -0200

    ip, langague, software

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var whoami = {};
+	whoami.ipaddress = req.ip;
+	whoami.language = req.headers["accept-language"].split(',')[0];
+	whoami.software = req.headers["user-agent"].split('(')[1].split(')')[0];
+  	res.status(200).json(whoami);
 });
 
 module.exports = router;
